3713d9b5b2624b829ba17e7d11f8719c17c3b02a
Bruno Garcia <bruno@brunogarcia.com>
2025-03-15 23:50:52 -0400

    bump dotnet sdk

--- Sentaur.Leaderboard.Web/Program.cs
+++ Sentaur.Leaderboard.Web/Program.cs
@@ -7,20 +7,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Blazor built-in integration is in a Draft: https://github.com/getsentry/sentry-dotnet/pull/2569/
-builder.Logging.AddSentry(o =>
+builder.UseSentry(o =>
 {
     o.Dsn = "https://8f3ccd6a8a8e5ba417de8df962236a7d@o87286.ingest.us.sentry.io/4506888120107008";
-    o.EnableTracing = true;
-
-    // System.PlatformNotSupportedException: System.Diagnostics.Process is not supported on this platform.
-    o.DetectStartupTime = StartupTimeDetectionMode.Fast;
-    // Warning: No response compression supported by HttpClientHandler.
-    o.RequestBodyCompressionLevel = CompressionLevel.NoCompression;
+    o.TracesSampleRate = 1.0;
 });
 
-builder.Services.AddScoped(sp => new HttpClient(
-    // Sentry tracing integration:
-    new SentryHttpMessageHandler()) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Logging.AddSentry(o => o.InitializeSdk = false);
+
+
+// builder.Services.AddScoped(sp => new HttpClient(
+//     // Sentry tracing integration:
+//     new SentryHttpMessageHandler()) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
